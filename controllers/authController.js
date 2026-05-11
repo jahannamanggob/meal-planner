d7eeb43014baa60093923ba45af6cb6cb9c5944c
jah <jahanna.manggob@gmail.com>
2026-05-12 04:09:56 +0800

    Add models layer and update controllers to follow MVC structure

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,28 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel'); 
+const User = require('../models/userModel');
 
 const SECRET = "secret123";
 
 exports.signup = async (req, res) => {
   const { email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const response = await User.create({ email, password: hashedPassword }); 
+  const response = await User.create({ email, password: hashedPassword });
   res.json(response.data);
 };
 
 exports.login = async (req, res) => {
-  const user = await User.findByEmail(req.body.email); // 👈 use model
-  if (!user) return res.status(400).json({ message: "User not found" });
+  try {
+    const user = await User.findByEmail(req.body.email);
+    if (!user) return res.status(400).json({ message: "User not found" });
 
-  const match = await bcrypt.compare(req.body.password, user.password);
-  if (!match) return res.status(400).json({ message: "Wrong password" });
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) return res.status(400).json({ message: "Wrong password" });
 
-  const token = jwt.sign({ id: user.id }, SECRET);
-  res.json({ token });
+    const token = jwt.sign({ id: user.id }, SECRET);
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 };
--- controllers/planController.js
+++ controllers/planController.js
@@ -1,21 +1,21 @@
-const Plan = require('../models/planModel'); 
+const Plan = require('../models/planModel');
 
 exports.getPlans = async (req, res) => {
-  const response = await Plan.getAll(); 
+  const response = await Plan.getAll();
   res.json(response.data);
 };
 
 exports.addPlan = async (req, res) => {
-  const response = await Plan.create(req.body); 
+  const response = await Plan.create(req.body);
   res.json(response.data);
 };
 
 exports.updatePlan = async (req, res) => {
-  const response = await Plan.update(req.params.id, req.body); 
+  const response = await Plan.update(req.params.id, req.body);
   res.json(response.data);
 };
 
 exports.deletePlan = async (req, res) => {
-  await Plan.remove(req.params.id); 
+  await Plan.remove(req.params.id);
   res.json({ message: "Deleted successfully" });
 };
